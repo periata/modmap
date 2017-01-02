b90e50a7477d5ab81b232a4193c96d8c56dedcb2
Periata Breatta <periatabreatta@gmail.com>
2017-01-02 03:15:31 +0000

    JSONRepresentableFactories are able to generate string results.

--- modmap/src/main/java/uk/co/periata/modmap/JSONRepresentableFactory.java
+++ modmap/src/main/java/uk/co/periata/modmap/JSONRepresentableFactory.java
@@ -1,6 +1,23 @@
 package uk.co.periata.modmap;
 
+import java.io.IOException;
+
 public interface JSONRepresentableFactory
 {
 	JSONRepresentable representableFor(Object o);
+	
+	default String jsonStringFor (Object o)
+	{
+		StringBuilder builder = new StringBuilder ();
+		try
+		{
+			representableFor (o).appendTo (builder);
+		}
+		catch (IOException ignored)
+		{
+			// shouldn't happen as builder is not expected to be able to throw an IOException, which
+			// should be the only way this will happen.
+		}
+		return builder.toString ();
+	}
 }
